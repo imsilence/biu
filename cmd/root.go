8846de5dab44b1161c7dcc1f8a4dc996c563a8c1
silence <imsilence@outlook.com>
2019-05-22 23:08:29 +0800

    [modify]: 添加结果输出到csv文件
    Signed-off-by: silence <imsilence@outlook.com>

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,7 @@ package cmd
 
 import (
 	"bufio"
+	"encoding/csv"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -34,7 +35,8 @@ var rootCmd = &cobra.Command{
 			logrus.SetLevel(logrus.DebugLevel)
 		}
 
-		file, err := os.Create(filepath.Join(config.HOME, "logs", "biu.log"))
+		file, err := os.Create(filepath.Join(config.CWD, "logs", "biu.log"))
+		// file, err := os.Create(filepath.Join(config.HOME, "logs", "biu.log"))
 		if err != nil {
 			return err
 		}
@@ -52,16 +54,33 @@ var rootCmd = &cobra.Command{
 		fmt.Printf("Total Plugin: %5d,    Query Plugins: %5d\n", len(manager.Plugins), len(plugins))
 
 		if query {
-			tpl := "| %010s.%-50s |"
-			fmt.Println(strings.Repeat("=", len(fmt.Sprintf(tpl, "", ""))))
-			for _, p := range plugins {
-				fmt.Printf(tpl, p.Id, p.Name)
-				fmt.Println()
+			tpl := "| %-8s | %-50s | %-100s |"
+			fmt.Println(strings.Repeat("=", len(fmt.Sprintf(tpl, "", "", ""))))
+			fmt.Printf(tpl, "ID", "Name", "Desc")
+			fmt.Println()
+			fmt.Println(strings.Repeat("-", len(fmt.Sprintf(tpl, "", "", ""))))
+			if len(plugins) > 0 {
+				for _, p := range plugins {
+					fmt.Printf(tpl, p.Id, p.Name, p.Desc)
+					fmt.Println()
+				}
+			} else {
+				fmt.Printf("| %-[1]*[2]s |\n", len(fmt.Sprintf(tpl, "", "", ""))-4, "Empty")
 			}
-			fmt.Println(strings.Repeat("=", len(fmt.Sprintf(tpl, "", ""))))
-		} else {
-			targets := manager.ParseTargets(host, cidr)
 
+			fmt.Println(strings.Repeat("=", len(fmt.Sprintf(tpl, "", "", ""))))
+		} else {
+			reportPath := filepath.Join(config.CWD, "reports", fmt.Sprintf("%s.csv", time.Now().Format("2006-01-02_15-04-05")))
+			reportFile, err := os.Create(reportPath)
+			if err != nil {
+				return err
+			}
+			defer reportFile.Close()
+			reportWriter := csv.NewWriter(reportFile)
+			defer reportWriter.Flush()
+			reportWriter.Write([]string{"URL", "Plugin ID", "Plugin Name", "Plugin Desc"})
+
+			targets := manager.ParseTargets(host, cidr)
 			fmt.Printf("Total Target: %5d\n", len(targets))
 
 			logrus.WithFields(logrus.Fields{
@@ -73,14 +92,29 @@ var rootCmd = &cobra.Command{
 			fmt.Println(strings.Repeat("=", len(fmt.Sprintf(tpl, "", ""))))
 			fmt.Printf("| %-133s |\n", "Results:")
 			fmt.Println(strings.Repeat("-", len(fmt.Sprintf(tpl, "", ""))))
+			empty := true
+
+			// file, err := os.Create(filepath.Join(config.HOME, "logs", "biu.log"))
+			if err != nil {
+				return err
+			}
+			defer file.Close()
+			writer := bufio.NewWriter(file)
+			defer writer.Flush()
+			logrus.SetOutput(writer)
+
 			for result := range results {
 				if r, ok := result.(plugin.Result); ok && !r.Safety {
+					reportWriter.Write([]string{r.Target.Raw, r.Plugin.Id, r.Plugin.Name, r.Plugin.Desc})
+					empty = false
 					fmt.Printf(tpl, r.Plugin.Name, r.Target.Raw)
 					fmt.Println()
 				}
 			}
+			if empty {
+				fmt.Printf("| %-133s |\n", "Empty")
+			}
 			fmt.Println(strings.Repeat("=", len(fmt.Sprintf(tpl, "", ""))))
-
 		}
 
 		end := time.Now()
@@ -98,8 +132,8 @@ func Execute() {
 
 func init() {
 	rootCmd.PersistentFlags().IntVarP(&worker, "worker", "w", 10000, "worker")
-	rootCmd.Flags().StringVarP(&host, "host", "H", "", "target host addr")
-	rootCmd.Flags().StringVarP(&cidr, "cidr", "c", "", "target cidr addr")
+	rootCmd.Flags().StringVarP(&host, "host", "H", "", "target host addr(127.0.0.1)")
+	rootCmd.Flags().StringVarP(&cidr, "cidr", "c", "", "target cidr addr(192.168.0.0/24)")
 	rootCmd.Flags().StringVarP(&pluginName, "plugin", "p", "*", "plugin")
 	rootCmd.Flags().BoolVarP(&query, "query", "q", false, "query plugin")
 	rootCmd.Flags().BoolVarP(&verbose, "verbose", "v", false, "verbose run log")
